3450eb773cd1a850cf289db931be2b05855e60be
Sixta <nicolassixta@gmail.com>
2021-03-22 14:40:25 -0400

    added nav bar (dropdown list has an error where text cannot be seen)

--- app/views/pages/Login.php
+++ app/views/pages/Login.php
@@ -1,21 +1,21 @@
 <?php include_once APP_ROOT . '/views/includes/header.php'; ?>
+<?php include APP_ROOT . '/views/includes/nav.php'; ?>
 
 <div class="login-container d-flex align-items-center justify-content-center">
     <div class="left p-1 ps-4 mx-5">
-        <p class="text-uppercase text-shadow m-0"> 
-            join us and build  
+        <p class="text-uppercase text-shadow m-0">
+            join us and build
             <br>your dream pc
         </p>
-        <a href="<?= URL_ROOT ?>/products" class="btn text-center mt-4 text-light shadow-sm">View Products</a> 
+        <a href="<?= URL_ROOT ?>/products" class="btn text-center mt-4 text-light shadow-sm">View Products</a>
     </div>
     <div class="right p-4 pt-5 ms-4">
         <div class='d-flex align-items-center'>
-            <h3 class="d-inline m-0">Login</h3> 
+            <h3 class="d-inline m-0">Login</h3>
             <span class='text-danger ms-3'> <?= $data['emptyFields'] ?? '' ?> </span>
-        </div>  
+        </div>
         <form method='post' class='mt-4'>
-            <input type="text" class="input form-control" placeholder="Email or Username" name="emailOrUser"
-                value="<?= $data['emailOrUser'] ?? '' ?>">
+            <input type="text" class="input form-control" placeholder="Email or Username" name="emailOrUser" value="<?= $data['emailOrUser'] ?? '' ?>">
             <input type="text" class="input form-control mt-2" placeholder="Password" name="pass">
 
             <button type='submit' class='submit-button p-2 w-100 d-block mt-4 btn btn-dark'><span>Login</span></button>
