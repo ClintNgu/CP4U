013ec1ebabe2028a381944c8a360400f7226480d
Sixta <nicolassixta@gmail.com>
2021-03-22 16:36:55 -0400

    Edited profile page

--- app/views/pages/Profile.php
+++ app/views/pages/Profile.php
@@ -1,18 +1,25 @@
 <?php include_once APP_ROOT . '/views/includes/header.php'; ?>
 
 <div class="profile-container-div">
-  <img src="https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg" alt="avatar-template">
-  <label>Name</label>
-  <label>Username</label>
-  <button class="edit-profile-button">Edit Profile</button>
+  <img class="profile-image" src="https://365psd.com/images/istock/previews/1009/100996291-male-avatar-profile-picture-vector.jpg" alt="avatar-template">
+  <br>
+  <label class="name-label">Name</label>
+  <br>
+  <label class="username-label">Username</label>
+  <br>
+  <button class='edit-profile-button p-2 d-block mt-4 btn btn-dark'><span>Edit Profile</span></button>
+  <br>
   <div class="edit-section-div">
     <input class="form-control" type="text" placeholder="Username" name='username' value="<?= $data['username'] ?? '' ?>">
-
+    <br>
     <input class="form-control" type="text" placeholder="Address" name='address' value="<?= $data['address'] ?? '' ?>">
-
+    <br>
     <input class="form-control" type="text" placeholder="Password" name='password' value="<?= $data['password'] ?? '' ?>">
-    <button>Save</button>
-    <button>Cancel</button>
+    <br>
+    <input class="form-control" type="text" placeholder="Confirm Password" name='confirmPassword'>
+    <br>
+    <button class="save-button">Save</button>
+    <button class="cancel-button">Cancel</button>
   </div>
 </div>
 
