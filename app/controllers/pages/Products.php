26f38a9e0179fe7aaefb498f8567b423214d5e59
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-03-23 00:26:54 -0400

    display products

--- app/controllers/pages/Products.php
+++ app/controllers/pages/Products.php
@@ -5,21 +5,30 @@ use app\controllers\Product;
 
 class Products extends Controller {
   private $data = ['title' => 'Products'];
+  private $productCtrl;
+   
 
+  public function __construct() {
+    $this->productCtrl = new Product;
+  }
+  
   public function index($params) {
-
-    echo '<pre>';
-    var_dump($_SESSION['User']);
-    echo '</pre>';
     
+    // echo '<pre>';
+    // var_dump($_SESSION['User']);
+    // echo '</pre>';
+    
+    //query all products
+    $products = $this->productCtrl->getProducts();
+    $this->data['products'] =  $products;
+
     //render view
     $this->renderView('Products', $this->data);
   } 
   
   public function product($params) {
     // query item 
-    $productCtrl = new Product;
-    $product = $productCtrl->getProduct($params[0] ?? -1);
+    $product = $this->productCtrl->getProduct($params[0] ?? -1);
 
     //item does not exist
     if (!$product) {
--- app/views/pages/Products.php
+++ app/views/pages/Products.php
@@ -1,5 +1,10 @@
 <?php include_once APP_ROOT . '/views/includes/header.php'; ?>
 
-products page
+<!-- display products -->
+<?php 
+  foreach ($data['products'] as $product) {
+    echo $product['item_id']. ": " . $product['item_name'] . '<br>';
+  } 
+?>
 
 <?php include_once APP_ROOT . '/views/includes/footer.php'; ?>
