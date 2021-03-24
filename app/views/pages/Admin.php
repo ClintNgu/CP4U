bd49495f1a537f79044cce69f8cbf1285b998219
Sixta <nicolassixta@gmail.com>
2021-03-24 13:36:53 -0400

    added admin page and nav bar can go to that page

---
+++ app/views/pages/Admin.php
@@ -0,0 +1,12 @@
+<?php include_once APP_ROOT . '/views/includes/header.php'; ?>
+
+<div class="container">
+
+  <label class="form-label">placeholder</label>
+  <input class="form-control" type="text" placeholder="" name='' value="">
+  <br>
+  <button type='submit' class='submit-button p-2 w-100 d-block mt-4 btn btn-dark'><span>Save Changes</span></button>
+
+</div>
+
+<?php include_once APP_ROOT . '/views/includes/footer.php'; ?>
