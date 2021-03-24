6ea089ce8421a42309797d4914c9f3a81fc9f00c
Sixta <nicolassixta@gmail.com>
2021-03-24 19:33:44 -0400

    edited individual product page

--- app/views/pages/Product.php
+++ app/views/pages/Product.php
@@ -1,8 +1,20 @@
 <?php include_once APP_ROOT . '/views/includes/header.php'; ?>
-
 <?php echo '<pre>';
 var_dump($data);
-echo '</pre>'; ?>
- 
+echo '</pre>';
+?>
+<div class="product-container">
+  <div class="product-left-div">
+    <img width="350" height="350">
+  </div>
+  <div class="product-right-div">
+    <h3>Item Name</h3>
+    <p>Item Discription</p>
+    <h4>Item Price</h4>
+    <label>Quantity: <span>0</span></label>
+    <button><span>Add to Cart </span></button>
+  </div>
+</div>
+
 
 <?php include_once APP_ROOT . '/views/includes/footer.php'; ?>
