11f8946ddc4880d1c7fe32edfd67a820d485b318
Sixta <nicolassixta@gmail.com>
2021-03-24 13:22:51 -0400

    edited colors

--- app/views/pages/Products.php
+++ app/views/pages/Products.php
@@ -1,66 +1,45 @@
 <?php include_once APP_ROOT . '/views/includes/header.php'; ?>
 
-<div class="d-flex flex-column p-2" style="width: 250px; height: 100vh;">
+<div class="sidebar d-flex flex-column p-2" style="width: 200px; height: 100vh;">
   <ul class="nav nav-pills flex-column mb-auto">
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#speedometer2"></use>
-        </svg>
         <img src='https://seeklogo.com/images/A/amd-logo-427958DBFB-seeklogo.com.png' width="30%" height="30%" alt="amd"> AMD
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#table"></use>
-        </svg>
         <img src='https://seeklogo.com/images/I/intel-logo-A05550B04C-seeklogo.com.png' width="30%" height="30%" alt="intel"> INTEL
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#grid"></use>
-        </svg>
         <img src='https://www.angadikart.com/media/122/catalog/asrock%20logo.jpg' width="30%" height="30%" alt="asrock"> ASROCK
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#grid"></use>
-        </svg>
         <img src='https://www.logolynx.com/images/logolynx/19/19b62ee44571eee229e1d94592efa3c3.png' width="30%" height="30%" alt="asus"> ASUS
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#people-circle"></use>
-        </svg>
         <img src='https://cdn.worldvectorlogo.com/logos/msi-gaming.svg' width="30%" height="30%" alt="msi"> MSI
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#people-circle"></use>
-        </svg>
         <img src='https://d1yjjnpx0p53s8.cloudfront.net/styles/logo-thumbnail/s3/0024/9901/brand.gif?itok=oGsQ0XeM' width="30%" height="30%" alt="gigabyte"> GIGABYTE
       </a>
     </li>
     <hr>
     <li class="sidebar-li">
       <a href="#" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16">
-          <use xlink:href="#people-circle"></use>
-        </svg>
         <img src='https://icon-library.com/images/corsair-icon/corsair-icon-23.jpg' width="30%" height="30%" alt="corsair"> CORSAIR
       </a>
     </li>
