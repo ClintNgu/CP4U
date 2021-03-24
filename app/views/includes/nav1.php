aa13b630797a809f5465a37a724e336476d1c1e2
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-03-24 01:52:31 -0400

    navbar dropdown color change

--- app/views/includes/nav1.php
+++ app/views/includes/nav1.php
@@ -4,13 +4,20 @@
       <a class="nav-link m-0 productText dropdown-toggle"
       href="<?= URL_ROOT ?>/products">Products</a>
       <ul class="dropdown-menu">
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/CPUs">CPUs</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/GPUs">GPUs</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/Motherboards">Motherboards</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/Rams">RAM Memory</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/M2s">Storage</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/Power_Supplies">Power Supplies</a></li>
-        <li><a class="dropdown-item py-2 border-bottom" href="<?= URL_ROOT ?>/products/CPU_Coolers">CPU Coolers</a></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/CPUs">CPUs</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/GPUs">GPUs</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/Motherboards">Motherboards</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/Rams">RAM Memory</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/M2s">Storage</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/Power_Supplies">Power Supplies</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/products/CPU_Coolers">CPU Coolers</a></li>
+        <li><hr class="dropdown-divider"></li>
         <li><a class="dropdown-item pt-2" href="<?= URL_ROOT ?>/products/PC_Cases">PC Cases</a></li>
       </ul>
     </div>
@@ -26,12 +33,12 @@
         <div class="nav-item dropdown"> 
           <a class="nav-link dropdown-toggle m-0"><i class="far fa-user-circle"></i></a>
           <ul class="profile-dropdown dropdown-menu dropdown-menu-end me-2 text-end">
-            <li><a class="dropdown-item" href="#">Admin</a></li>
-            <li><a class="dropdown-item" href="<?= URL_ROOT ?>/profile">My Profile</a></li>
-            <li><a class="dropdown-item" href="#">My Orders</a></li>
+            <li><a class="dropdown-item py-2" href="#">Admin</a></li>
+            <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/profile">My Profile</a></li>
+            <li><a class="dropdown-item py-2" href="#">My Orders</a></li>
             <li><hr class="dropdown-divider"></li>
             <!-- check if user isLoggedIn -->
-            <li><a class="dropdown-item" href="<?= URL_ROOT ?>/login">Sign In</a></li>
+            <li><a class="dropdown-item py-2" href="<?= URL_ROOT ?>/login">Sign In</a></li>
           </ul>
         </div>
       </li>
