f0a51d19232ddeaacdcbec676de46f679ea6f2b1
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-03-27 02:54:50 -0400

    moved scripts to end of body

--- app/views/includes/footer.php
+++ app/views/includes/footer.php
@@ -17,11 +17,9 @@
   </div>
 </footer>
 
-<!-- bootstrap js -->
+<!-- js -->
+<script src='<?= URL_ROOT ?>/js/index.js'></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+
 </body>
-
-<!-- scroll up btn js -->
-<script type='text/javascript' src='<?= URL_ROOT ?>/js/index.js'></script>
-
 </html>
