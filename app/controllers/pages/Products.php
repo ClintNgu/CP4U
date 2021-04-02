6b24faf3f6d271b39670ca583886dd01e9da93da
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-04-02 00:16:09 -0400

    search for  bug fix

--- app/controllers/pages/Products.php
+++ app/controllers/pages/Products.php
@@ -78,7 +78,11 @@ class Products extends Controller
   }
 
   private function filterSearch($products, $searchVal) {
-    $searchVal = str_replace('$', '', $searchVal);
+    $searchVal = $searchVal === '$' ? '' : $searchVal;  
+    $searchVal = isset($searchVal[0]) && $searchVal[0] === '$' && 
+                 isset($searchVal[1]) && is_numeric($searchVal[1]) 
+                 ? substr($searchVal, 1) : $searchVal;
+    
     $filtered = array_filter($products, function ($product) use ($searchVal) {
         if (
           str_contains(strtolower($product['item_name']), strtolower($searchVal)) ||
