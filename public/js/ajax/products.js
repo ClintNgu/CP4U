c408e157137592b9245bdc946d52b5cc217db03f
Clinton Ng <nguyennoelclinton1@gmail.com>
2021-04-02 01:41:38 -0400

    ajax code clean up

--- public/js/ajax/products.js
+++ public/js/ajax/products.js
@@ -12,14 +12,6 @@ const LOAD_COUNT = 8;
 let showItemsCount;
 let items;
 
-// init items
-const initItems = () => {
-  items = [...document.querySelectorAll('.item-wrapper')];
-  showItemsCount = LOAD_COUNT;
-  loadMoreBtn.classList.remove('d-none');
-  showItems(items, showItemsCount);
-};
-
 // Spinner
 const toggleSpinner = () => {
   spinner.classList.toggle('d-none');
@@ -27,48 +19,7 @@ const toggleSpinner = () => {
   $('html, body').animate({ scrollTop: 60 }, 100);
 };
 
-// Search Box
-searchBox.oninput = async (e) => {
-  const { value } = e.target;
-  const data = new FormData();
-  data.append('searchVal', JSON.stringify(value));
-
-  const response = await fetch(window.location.href, {
-    method: 'POST',
-    body: data,
-  });
-
-  productItem.innerHTML = await response.text();
-  initItems();
-};
-
-// Sidebar Filter Ajax
-const findChecked = (inputs = []) => {
-  let filtered = inputs.filter(({ checked }) => checked);
-  filtered = (filtered.length === 0 ? inputs : filtered).map(({ value }) => value.toLowerCase());
-  return filtered;
-};
-
-filterBtn.onclick = async () => {
-  let suppliers = findChecked(supplierInputs);
-  let prices = findChecked(priceInputs);
-  toggleSpinner();
-
-  const data = new FormData();
-  data.append('suppliers', JSON.stringify(suppliers));
-  data.append('prices', JSON.stringify(prices));
-  data.append('searchVal', JSON.stringify(searchBox.value));
-  const response = await fetch(window.location.href, {
-    method: 'POST',
-    body: data,
-  });
-
-  setTimeout(toggleSpinner, 700);
-  productItem.innerHTML = await response.text();
-  initItems();
-};
-
-// Load More Btn
+// init items
 const showItems = (items, count) => {
   const { length } = items;
 
@@ -82,6 +33,47 @@ const showItems = (items, count) => {
   }
 };
 
+const initItems = () => {
+  items = [...document.querySelectorAll('.item-wrapper')];
+  showItemsCount = LOAD_COUNT;
+  loadMoreBtn.classList.remove('d-none');
+  showItems(items, showItemsCount);
+};
+
+const findChecked = (inputs = []) => {
+  let filtered = inputs.filter(({ checked }) => checked);
+  filtered = (filtered.length === 0 ? inputs : filtered).map(({ value }) => value.toLowerCase());
+  return filtered;
+};
+
+const ajaxHandler = async () => {
+  let suppliers = findChecked(supplierInputs);
+  let prices = findChecked(priceInputs);
+
+  const data = new FormData();
+  data.append('suppliers', JSON.stringify(suppliers));
+  data.append('prices', JSON.stringify(prices));
+  data.append('searchVal', JSON.stringify(searchBox.value));
+  const response = await fetch(window.location.href, {
+    method: 'POST',
+    body: data,
+  });
+
+  productItem.innerHTML = await response.text();
+  initItems();
+};
+
+// search box
+searchBox.oninput = ajaxHandler;
+
+// sidebar filter
+filterBtn.onclick = async () => {
+  toggleSpinner();
+  await ajaxHandler();
+  setTimeout(toggleSpinner, 400);
+};
+
+// load more button
 loadMoreBtn.onclick = () => {
   showItemsCount += LOAD_COUNT;
   showItems(items, showItemsCount);
